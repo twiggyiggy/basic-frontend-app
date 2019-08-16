a6beadd80163729b3f33851a285d1fc01d90ba60
Marcel Wojdylo <marcelwojdylo@gmail.com>
2019-08-16 19:00:42 +0100

    start work on services/api-services.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ class Login extends Component {
 
     this.props.login({ username, password })
     .then( (user) => {
-      console.log(user)
+      // console.log(user)
     })
     .catch( error => console.log(error) )
   }
--- src/pages/Private.js
+++ src/pages/Private.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 import withAuth from '../components/withAuth.js';
 
 class Private extends Component {
+
+  
+
   render() {
     return (
       <div>
-        <h1>Welcome Paquito</h1>
+        <h1>Welcome {this.props.user.username}</h1>
       </div>
     )
   }
