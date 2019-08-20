01433d0583d0533efc579015dd7a435d52ac1fcd
Marcel Wojdylo <marcelwojdylo@gmail.com>
2019-08-20 20:24:16 +0200

    Make changes to styling

--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { withRouter, Redirect } from 'react-router-dom' 
 import { thisExpression } from '@babel/types';
 
+import Navbar from '../components/Navbar.js'
+
 class Setup extends Component {
 
   wasFormSubmitted = false;
@@ -15,6 +17,7 @@ class Setup extends Component {
 
   handleChange = (event) => {
     const { name, value } = event.target;
+    console.log(name,value)
     this.setState({
       [name]: value,
     })
@@ -46,56 +49,56 @@ class Setup extends Component {
   }
 
   setUpForm =
-    <form onSubmit={this.handleSubmit}>
+    <form onSubmit={this.handleSubmit} className="setup-container">
       <p>What would you like to sketch?</p>
-      <div className='radio-container'>
-        <label htmlFor='hands'>Hands</label>
-        <input type='radio' id='hands' name='category' value='hands' onChange={this.handleOnChange}/>
+      <div>
+        <input type='radio' id='hands' name='category' value='hands' onChange={this.handleChange}/>
+        <label htmlFor='hands'>hands</label>
         
-        <label htmlFor='feet'>Feet</label>
-        <input type='radio' id='feet' name='category' value='feet' onChange={this.handleOnChange}/>
+        <input type='radio' id='feet' name='category' value='feet' onChange={this.handleChange}/>
+        <label htmlFor='feet'>feet</label>
         
-        <label htmlFor='face'>Face</label>
-        <input type='radio' id='face' name='category' value='face' onChange={this.handleOnChange}/>
+        <input type='radio' id='face' name='category' value='face' onChange={this.handleChange}/>
+        <label htmlFor='face'>face</label>
 
-        <label htmlFor='figure'>Figure</label>
-        <input type='radio' id='figure' name='category' value='figure' onChange={this.handleOnChange}/>
+        <input type='radio' id='figure' name='category' value='figure' onChange={this.handleChange}/>
+        <label htmlFor='figure'>figure</label>
       
-        <label htmlFor='other'>Other</label>
-        <input type='radio' id='other' name='category' value='other' onChange={this.handleOnChange}/>
+        <input type='radio' id='other' name='category' value='other' onChange={this.handleChange}/>
+        <label htmlFor='other'>other</label>
       
-        <label htmlFor='all'>All</label>
-        <input type='radio' id='all' name='category' value='all' onChange={this.handleOnChange}/>
+        <input type='radio' id='all' name='category' value='all' onChange={this.handleChange}/>
+        <label htmlFor='all'>all</label>
       </div>
 
       <p>How long would you like to see each photo for?</p>
-      <div className='radio-container'>
+      <div>
+        <input type='radio' id='30000' name='interval' value='30000' onChange={this.handleChange}/>
         <label htmlFor='30000'>30 seconds</label>
-        <input type='radio' id='30000' name='interval' value='30000' />
 
+        <input type='radio' id='60000' name='interval' value='60000' onChange={this.handleChange}/>
         <label htmlFor='60000'>60 seconds</label>
-        <input type='radio' id='60000' name='interval' value='60000' />
 
+        <input type='radio' id='120000' name='interval' value='120000' onChange={this.handleChange}/>
         <label htmlFor='120000'>2 minutes</label>
-        <input type='radio' id='120000' name='interval' value='120000' />
 
+        <input type='radio' id='180000' name='interval' value='180000' onChange={this.handleChange}/>
         <label htmlFor='180000'>3 minutes</label>
-        <input type='radio' id='180000' name='interval' value='180000' />
 
+        <input type='radio' id='300000' name='interval' value='300000' onChange={this.handleChange}/>
         <label htmlFor='300000'>5 minutes</label>
-        <input type='radio' id='300000' name='interval' value='300000' />
       </div>
 
       <p>How many photos would you like to sketch?</p>
-      <div className='radio-container'>
+      <div>
+        <input type='radio' id='10' name='numberOfPhotos' value='10' onChange={this.handleChange}/>
         <label htmlFor='10'>10</label>
-        <input type='radio' id='10' name='numberOfPhotos' value='10' />
 
+        <input type='radio' id='15' name='numberOfPhotos' value='15' onChange={this.handleChange}/>
         <label htmlFor='15'>15</label>
-        <input type='radio' id='15' name='numberOfPhotos' value='15' />
 
+        <input type='radio' id='20' name='numberOfPhotos' value='20' onChange={this.handleChange}/>
         <label htmlFor='20'>20</label>
-        <input type='radio' id='20' name='numberOfPhotos' value='20' />
       </div>
 
       <button type='submit'>Start session</button>
@@ -117,6 +120,7 @@ class Setup extends Component {
           ? this.redirectToSlideshow
           : this.setUpForm
         }
+        <Navbar/>
       </>
     )
   }
