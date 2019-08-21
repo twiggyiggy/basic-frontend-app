8d16a1810ec99b3705090041ae618560b911effe
Marcel Wojdylo <marcelwojdylo@gmail.com>
2019-08-21 15:36:39 +0200

    fix issue with null entries to slideshow

--- src/pages/Setup.js
+++ src/pages/Setup.js
@@ -27,7 +27,10 @@ class Setup extends Component {
     const user = await authService.getCurrentUser()
     const response = await apiService.getUserPhotos(user)
     const userPhotos = response.data
-    const userPhotoUrls = userPhotos.map(photoObj => photoObj.imageUrl)
+    const userPhotoUrls = userPhotos
+      .filter(photoObj => photoObj ? true : false)
+      .map(photoObj => photoObj.imageUrl)
+    
     this.setState({
       wasFormSubmitted: true,
       photosFromUser: userPhotoUrls,
@@ -97,21 +100,12 @@ class Setup extends Component {
 
       <button type='submit'>Start session</button>
     </form>
-
-
-      // vvv this should be removed once Slideshow starts receiving props.photos and props.iterationLength
-    mockPhotos = [
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Foot_on_white_background.jpg/345px-Foot_on_white_background.jpg',
-        'https://upload.wikimedia.org/wikipedia/commons/3/32/Human-Hands-Front-Back.jpg',
-        'https://upload.wikimedia.org/wikipedia/en/e/e8/Samfacejr.jpg',
-        'https://static.turbosquid.com/Preview/2015/02/17__07_42_52/Leg_render_1.jpg5f4dfaef-f39c-43ea-afe2-522e3788b169Original.jpg',
-        'https://media.ottobock.com/_web-site/prosthetics/upper-limb/silicone-cover/images/_35236_dsc0088_169_4c_wb_1_1_hotspot_zoom.jpg'
-    ]
     
     
 
   render() {
     const { photosFromUser } = this.state;
+    console.log(photosFromUser);
     return (
       <>
         {
@@ -119,7 +113,6 @@ class Setup extends Component {
           ? <Redirect to={{
             pathname: '/slideshow',
             state: {
-              photos: this.mockPhotos,
               iterationLength: this.state.interval,
               photosFromUser: photosFromUser,
             } 
