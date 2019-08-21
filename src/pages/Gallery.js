94c686bdbe5ac3c4f8779096253885f82235ee39
Ignacy Wojdylo <ignacy.wojdylo@gmail.com>
2019-08-21 12:39:30 +0200

    add addphoto form in component, updating photos in gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,15 +2,42 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import UserPhotos from '../components/UserPhotos'
 import AddPhoto from '../components/AddPhoto';
+import apiService from '../services/api-service.js'
+import authService from '../services/auth-service'
 
-function Gallery() {
-  return (
-    <div className="gallery-container">
-      <AddPhoto />
-      <UserPhotos/>
+class Gallery extends React.Component {
+  state = {
+    userPhotos: []
+  }
+
+  getGalleryPhotos = async () => {
+    const user = await authService.getCurrentUser()
+    .then(response => response)
+    
+    apiService.getUserPhotos(user)
+    .then(response => (
+        this.setState({
+            userPhotos: response.data.reverse()
+        })
+    ))
+  }
+
+
+  componentDidMount() {
+    this.getGalleryPhotos();
+  }
+
+  render() {
+    const {userPhotos} = this.state;
+
+    return (
+      <div className="gallery-container">
+      <AddPhoto getUpdatedGalleryPhotos={this.getGalleryPhotos} />
+      <UserPhotos userPhotos={userPhotos}/>
       <Navbar/>
     </div>
   )
+}
 }
 
 export default Gallery
